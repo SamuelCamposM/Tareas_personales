8a24aa256e4bc668fd11833e085d1f8eb4430ace
samuel campos <samuelcampos720@ooutlook.com>
2020-06-08 14:06:58 -0600

    first comm

--- src/index.js
+++ src/index.js
@@ -20,6 +20,6 @@ app.use('/tareas', require('./routes/tareas'))
 
 //appp en el puerto :
 app.set('port', process.env.PORT || 4000);
-app.listen(app.get('port'), ()=> {
+app.listen(app.get('port'),'0.0.0.0', ()=> {
     console.log('servidor en el puerto ', app.get('port'));
     })
